389b5ab66da3d2ec6eea890fde371bd41a6187c0
konvish <konvish@163.com>
2018-08-17 18:07:55 +0800

    抽象化所有限制信息

--- src/main/scala/cn/sibat/tma/CityCost.scala
+++ src/main/scala/cn/sibat/tma/CityCost.scala
@@ -1,14 +1,89 @@
 package cn.sibat.tma
 
+import scala.collection.mutable
+
 /**
   * 自定义适应度函数
+  *
+  * @author kong
   */
 object CityCost {
+  //飞机的动态信息表,飞机code->info
+  private var aircraft: mutable.HashMap[String, String] = _
+  //od信息表，od-> info
+  private var dura_dist: mutable.HashMap[String, String] = _
+  //酒店机场信息表
+  private var location: mutable.HashMap[String, String] = _
+
+  /**
+    * 设置飞机信息
+    * 如果信息太大就使用数据库作为替代
+    *
+    * @param aircraftMap 飞机信息
+    */
+  def setAircraft(aircraftMap: mutable.HashMap[String, String]): Unit = {
+    aircraft = aircraftMap
+  }
+
+  /**
+    * 设置酒店之间的距离信息
+    * 如果信息太大就使用数据库作为替代
+    *
+    * @param duraDist 酒店距离信息
+    */
+  def setDuraDist(duraDist: mutable.HashMap[String, String]): Unit = {
+    dura_dist = duraDist
+  }
+
+  /**
+    * 设置机场或者酒店的信息
+    * 如果信息太大就使用数据库作为替代
+    *
+    * @param locationMap 酒店或机场的信息
+    */
+  def setLocation(locationMap: mutable.HashMap[String, String]): Unit = {
+    location = locationMap
+  }
+
+  /**
+    * 设置所有的信息，包括飞机信息、酒店之间距离信息、机场酒店信息
+    * 如果信息太大就使用数据库作为替代
+    *
+    * @param aircraftMap 飞机信息
+    * @param duraDist    酒店之间距离信息
+    * @param locationMap 机场酒店信息
+    */
+  def setAll(aircraftMap: mutable.HashMap[String, String], duraDist: mutable.HashMap[String, String], locationMap: mutable.HashMap[String, String]): Unit = {
+    aircraft = aircraftMap
+    dura_dist = duraDist
+    location = locationMap
+  }
+
   def cityCost(cities: Array[CityTMA]): Double = {
     cities.length
   }
 
+  /**
+    * 适应度计算函数
+    *
+    * @param cities 基因集合
+    * @param indexs 组合策略
+    * @return
+    */
   def cityCost(cities: Array[CityTMA], indexs: Array[Int]): Double = {
+    /**
+      * 限制条件
+      * 1. 飞机日落半小时要落地。
+      * 2. 飞机可飞时长为3小时和5小时，可略微超过该时间。
+      * 3. 飞机负载，GTOW - APS = 行李重量 + 乘客重量 + 油重量
+      * 4. 机场酒店距离和时间信息表
+      * 5. 飞机停留时间，机场25min、酒店15min，加油+5min
+      * 6. 酒店停留飞机数量限制，加油，排队
+      * 7. 飞机续航里程限制，500lbs/h,油箱油量2000lbs。
+      * 8. 中转限制
+      * 9. 飞机数量限制，起始位置
+      *
+      */
     cities.length
   }
 
--- src/main/scala/cn/sibat/tma/CityTMA.scala
+++ src/main/scala/cn/sibat/tma/CityTMA.scala
@@ -2,6 +2,7 @@ package cn.sibat.tma
 
 /**
   * 基因编码
+  * 即需求的信息
   *
   * @param id              id标识一个需求
   * @param odType          该基因类型,o or d
@@ -15,3 +16,30 @@ package cn.sibat.tma
   * @param endTime         时间窗结束
   */
 case class CityTMA(id: String, odType: String, direction: String, name: String, legs: Int, seats: Int, passengerWeight: Double, baggageWeight: Double, startTime: String, endTime: String)
+
+/**
+  * 飞机属性
+  *
+  * @param code            飞机代码
+  * @param startFlyTime    开始飞行时间
+  * @param maxWeight       最大载重
+  * @param oilWeight       油量重量
+  * @param maxFlyTime      最大飞行时间
+  * @param maxSeats        最大座位
+  * @param useSeats        使用座位
+  * @param location        所处位置
+  * @param baggageWeight   行李重量
+  * @param passengerWeight 乘客重量
+  */
+case class Aircraft(code: String, startFlyTime: String, maxWeight: Double, oilWeight: Double, maxFlyTime: Int, maxSeats: Int, useSeats: Int, location: String, baggageWeight: Double, passengerWeight: Double)
+
+/**
+  * 酒店或者机场的信息
+  *
+  * @param code     酒店或者机场代码
+  * @param maxStand 最大停留数
+  * @param canOil   是否能加油
+  * @param legs     中转数
+  * @param useStand 使用的机坪
+  */
+case class Airport(code: String, maxStand: Int, canOil: Boolean, legs: Int, useStand: Int)
